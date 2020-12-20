3aaf8ae722a6f499cc1d645bab74cf63131ffc21
Michael Arvelo <michaeldevelopscode@gmail.com>
2020-12-19 19:54:31 -0500

    added function to create html

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs'); // allows app to access the fs module's functions through the fs assignment
 
-// Notice the lack of parentheses around the `profileDataArr` parameter?
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    }
-  
-    console.log('================');
-  
-    // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
+const profileDataArgs = process.argv.slice(2, process.argv.length); //grabs two arguments from console
+const [name, github] = profileDataArgs; // assigns the two args to vars
 
-};
+// return html with vars in place
+const generatePage = (name, github) => {
+    return `
+    <!DOCTYPE html> 
+    <html lang="en"> 
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Portfolio Demo</title>
+    </head>
   
-  printProfileData(profileDataArgs);
+    <body>
+      <h1>${name}</h1>
+      <h2><a href="https://github.com/${github}">Github</a></h2>
+    </body>
+    </html>
+    `;
+  };
+
+  // create the html file (defined in generatePage function) in file path
+  fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+  
+    console.log('Portfolio complete! Check out index.html to see the output!');
+  });
